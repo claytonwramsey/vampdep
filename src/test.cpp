7e058bb0a03648e1c107d90d0a20c7362b0145f4
Clayton Ramsey <claytonwramsey@gmail.com>
2024-10-16 15:54:03 -0500

    repro for vamp issues

---
+++ src/test.cpp
@@ -0,0 +1,8 @@
+#include <vamp/collision/environment.hh>
+#include <vamp/planning/rrtc.hh>
+
+int main(void) {
+    const vamp::planning::Environment env;
+    // todo everything else
+    return 0;
+}
